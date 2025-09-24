82be3cb4f80d651e5a475ef78ccb82c9b78a717e
Pritam Das <talkwithpritamdas@gmail.com>
2025-09-24 14:12:10 +0530

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,105 +1,77 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { BsTelegram,BsFacebook,BsInstagram,BsYoutube } from "react-icons/bs";
+import { BsTelegram, BsFacebook, BsInstagram, BsYoutube } from "react-icons/bs";
 
 function Footer() {
   return (
-    <div className="text-white font-[500] text-sm">
-      <div className="bg-[#00428A] top p-4 grid-col-12 lg:flex base:flex-col md:flex">
-        <div className="col-lg-5 col-md-4 footer-info">
-          <h3 className="font-[1000] text-3xl text-left py-2">Phoenix</h3>
-          <p className="lg:text-justify base:text-justify md:text-left">
-            Phoenix has always strived for overall development of an individual,
-            from technical to communication skills of a person by conducting
-            domain-specific forums to interpersonal development by working in a
-            team and taking leadership roles. Inculcating competitive spirit
-            among college students by organizing events and tech Fests for them
-            to display their skills, we do it all.
+    <footer className="text-white font-medium text-sm">
+      <div className="bg-[#00428A] p-8 flex flex-col lg:flex-row lg:justify-between gap-8">
+        
+        {/* About Section */}
+        <div className="lg:w-5/12">
+          <h3 className="font-extrabold text-3xl py-2">Phoenix</h3>
+          <p className="text-justify">
+            Phoenix strives for the overall development of students, from technical
+            skills to communication. We conduct domain-specific forums, tech fests,
+            and team activities to help students showcase their skills and develop
+            leadership qualities.
           </p>
         </div>
 
-        <div className="col-lg-3 col-md-4 text-left justify-center footer-links">
-          <h4 className="font-[700] text-xl py-2">Useful Links</h4>
-          <ul>
-            <li className="py-1">
-              <Link className="hover:text-slate-50" to="/home">
-                Home
-              </Link>
-            </li>
-            <li className="py-1">
-              <Link className="hover:text-slate-50" to="/wings">
-                Wings
-              </Link>
-            </li>
-            <li className="py-1">
-              <Link className="hover:text-slate-50" to="/events">
-                Events
-              </Link>
-            </li>
-            <li className="py-1">
-              <Link className="hover:text-slate-50" to="/core">
-                Core 2022-23
-              </Link>
-            </li>
-            <li className="py-1">
-              <Link className="hover:text-slate-50" to="/webteam">
-                Web team
-              </Link>
-            </li>
-            <li className="py-1">
-              <Link className="hover:text-slate-50" to="/contactus">
-                Contact Us
-              </Link>
-            </li>
+        {/* Useful Links */}
+        <div className="lg:w-3/12">
+          <h4 className="font-bold text-xl py-2">Useful Links</h4>
+          <ul className="space-y-2">
+            {[
+              { name: "Home", path: "/home" },
+              { name: "Wings", path: "/wings" },
+              { name: "Events", path: "/events" },
+              { name: "Core 2022-23", path: "/core" },
+              { name: "Web Team", path: "/webteam" },
+              { name: "Contact Us", path: "/contactus" },
+            ].map((link, idx) => (
+              <li key={idx}>
+                <Link
+                  to={link.path}
+                  className="hover:text-slate-50 transition-colors duration-200"
+                >
+                  {link.name}
+                </Link>
+              </li>
+            ))}
           </ul>
         </div>
 
-        <div className="col-lg-4 col-md-4 footer-contact">
-          <h4 className="font-[700] text-xl py-2">Contact Us</h4>
-          <p>
-            Netaji Subhash Engineering College, Garia, Panchpota, Kolkata, West
-            Bengal, 700152
+        {/* Contact Info */}
+        <div className="lg:w-4/12">
+          <h4 className="font-bold text-xl py-2">Contact Us</h4>
+          <p className="leading-relaxed">
+            Netaji Subhash Engineering College, Garia, Panchpota, Kolkata, West Bengal, 700152
             <br />
-            <strong>Email: info@phoenixnsec.in</strong>
-            <br />
+            <strong>Email:</strong> <a href="mailto:info@phoenixnsec.in" className="underline hover:text-slate-50">info@phoenixnsec.in</a>
           </p>
-          <div className="mt-2 flex social-links">
-            <a
-              target="_blank"
-              href="https://t.me/phoenix_nsec2020"
-              className="mx-1 telegram"
-            >
-              <BsTelegram size={'22px'} />
+          <div className="mt-4 flex space-x-3">
+            <a target="_blank" rel="noopener noreferrer" href="https://t.me/phoenix_nsec2020" className="hover:text-blue-300">
+              <BsTelegram size={22} />
             </a>
-            <a
-              target="_blank"
-              href="https://www.facebook.com/nsec.phoenix/"
-              className="mx-1 facebook"
-            >
-              <BsFacebook size={'22px'} />
+            <a target="_blank" rel="noopener noreferrer" href="https://www.facebook.com/nsec.phoenix/" className="hover:text-blue-600">
+              <BsFacebook size={22} />
             </a>
-            <a
-              target="_blank"
-              href="https://www.instagram.com/phoenix_nsec/"
-              className="mx-1 instagram"
-            >
-              <BsInstagram size={'22px'} />
+            <a target="_blank" rel="noopener noreferrer" href="https://www.instagram.com/phoenix_nsec/" className="hover:text-pink-400">
+              <BsInstagram size={22} />
             </a>
-            <a
-              target="_blank"
-              href="https://www.youtube.com/channel/UCBy1iIhw34E7YlHQ8tc4rDA"
-              className="mx-1 youtube"
-            >
-              <BsYoutube size={'26px'} />
+            <a target="_blank" rel="noopener noreferrer" href="https://www.youtube.com/channel/UCBy1iIhw34E7YlHQ8tc4rDA" className="hover:text-red-600">
+              <BsYoutube size={26} />
             </a>
           </div>
         </div>
-
       </div>
-      <p className="text-[.8rem] md:text-[1rem] bg-[#013a77] p-3 text-center">
-        &copy; Copyright Phoenix 2025. All Rights Reserved
-      </p>
-    </div>
+
+      {/* Bottom */}
+      <div className="bg-[#013a77] p-3 text-center text-[0.85rem] md:text-[1rem]">
+        &copy; 2025 Phoenix. All Rights Reserved
+      </div>
+    </footer>
   );
 }
 
